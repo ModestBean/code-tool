675b4245432bc6ca8deef218887ee842c105eee1
ModestBean <yindou97@163.com>
2019-10-02 19:17:56 +0800

    [update] radon直线检测和图案识别

--- work1001_1_1.m
+++ work1001_1_1.m
@@ -1,47 +1,76 @@
 % @author 憨豆酒 YinDou yindou97@163.com
-% @date 20191001
-% @description Radon变换
-clc;
-clear;
+% @date 20191002
+% @description radon直线检测 
+% 建议先阅读本节博客：
+% 本节实现的为检测一条直线，若想实现检测多条直线，需要计算多次最大值
+clc
+clear all;
+close all;
+
+% 读取原始图像
 I = fitsread('solarspectra.fts');
 I = mat2gray(I);
+figure(1),imshow(I);
 BW = edge(I);
-figure,imshow(I);
-% theta = 0:179;
-% [R, xp] = radon(BW, theta);
-% [M, N]=size(R); 
+[height, width ] = size (I);
+width_half = width / 2.0;
+height_half = height / 2.0;
 
-% 进行reshape
-x0 = 96.0;
-y0 = 83.0;
-thetaT = 45.0;
-k = tan(thetaT*pi/180.0); 
-b = y0-k*x0;
-k2 = -1/k;
+% 执行radon函数
+theta = 1:180;
+[R,xp] = radon(BW,theta);
+[M,N]=size(R);
+% reshape二维矩阵R，变换成一维向量
+J=reshape(R,M*N,1);
 
 
-syms x
-f = sqrt((k*x+b-y0)^2+(x-x0)^2);  %求出到中心点距离为d的点
-soln  = solve(f == 8,x);
-if soln(1)<96
+% 寻找最大值的索引
+% J排序，倒序
+sort_J = sort(J, 'descend');
+% 取向量J中最大值
+% 本节实现的为检测一条直线，若想实现检测多条直线，需要计算第二大值，第三大值，第.....
+max_value = sort_J(1);
+% 得到在矩阵索引，在数组中的索引
+[row,col] = find(R == max_value);
+
+% 将数组索引变换成theta和distance，详情见：
+max_line_theta = col;
+max_line_distance = row - 129;
+
+
+% k过图像中心点直线斜率，k2为检测直线斜率
+k = tan(max_line_theta*pi/180.0);
+k2 = -1.0/k;
+% 坐标系原点
+x0 = 0.0;
+y0 = 0.0;
+
+% 计算两条直线交点
+% 定义变量x
+syms x 
+d = sqrt((k*x-y0)^2+(x-x0)^2);
+soln  = solve(d == abs(max_line_distance),x);
+% radon的峰值在图像左侧，所以判断。 不同的图像注意区分，如若使用此代码记得修改
+if soln(1)<=0
     xT = soln(1);
 else
     xT = soln(2);
 end
+yT = k*xT;
 
-yT = k*xT + b;
-b2 = yT-k2 * xT; %得到直线截距
-x1 = 0; %任意求出直线上两点，用于画线
-y1 = k2*x1+b2;
-x2 = 60;
-y2 = k2*x2+b2;
-hold on
-line([x1,x2],[y1,y2],'Color','r','LineWidth',2)
+% 检测的直线与图像上下的交点，涉及到图像坐标系变换，详情见博客：
+y1 = 0.0;
+x1 = (height_half - y1 - yT)/k2 + xT + width_half;
+y2 = 165.0;
+x2 = (height_half - y2 - yT)/k2 + xT + width_half;
+hold on;
+% 绘制直线
+line([x1, x2],[y1,y2],'color','r','LineWidth',1); 
 
-
-% J=reshape(R, M*N, 1); 
-% [PKS,LOCS]= findpeaks(J,'THRESHOLD',23);
-% for i = 1:5
-%     d = mod(LOCS(i),M)-(M-1)/2;       
-%     theta = fix(LOCS(i)/M);
-% end
+% 绘制randon图
+figure(2)
+imagesc(theta, xp, R); colormap(hot);
+xlabel('\theta (degrees)');
+ylabel('x^{\prime} (pixels from center)');
+title('R_{\theta} (x^{\prime})');
+colorbar
--- work1001_2_1.m
+++ work1001_2_1.m
@@ -1,19 +1,21 @@
 % @author 憨豆酒 YinDou yindou97@163.com
-% @date 20191001
-% @description 相关操作 识别字母
+% @date 20191002
+% @description 相关和傅里叶变换 从图像中读取图案
 clc;
-clear;
-% 加载初始照片
+clear all;
+close all;
+% 读取logo图像
 I = imread('logo.png'); 
 I = rgb2gray(double(I));
 [ni,mi]=size(I);
 figure(1);imshow(I,'InitialMagnification','fit'); title('Original Image');
+% 读取字母图像
 J = imread('pattern.png'); 
 J = rgb2gray(double(J));
 [nj,mj]=size(J);
 figure(2); imshow(J,'InitialMagnification','fit'); title('Image');
 
-% 规划到同样大小
+% 将两幅图像规定到同一大小，填充边缘为黑色
 I(:,mi:mi+mj)=0;
 I(ni:ni+nj,:)=0;
 figure(3);imshow(I,'InitialMagnification','fit'); title('Original Image');
@@ -25,11 +27,16 @@ figure(4); imshow(J,'InitialMagnification','fit'); title('Image');
 [m,n]=size(I);
 
 % 寻找字母位置
+% 对I和J分别执行傅里叶变换
 FI=fft2(double(I*255));
 FJ=fft2(double(J*255));
+% 对傅里叶J进行相关操作
 CFJ=conj(FJ);
+% 将傅里叶I和相关后的傅里叶J相乘
 F=FI.*CFJ;
+% 计算F的逆傅里叶变换
 R=ifft2(F);
+% 规则化R图像，使小于0的等于0
 for i=1:ni
     for j=1:mi
         if(R(i,j)<0) 
@@ -37,20 +44,25 @@ for i=1:ni
         end
     end
 end
+% 规则化矩阵R，让矩阵R的每一个值除以最大值
+% max(max(R))为矩阵R的最大值
+% 第一个max计算每一列最大值，第二个max计算每一列最大值中的最大值
 R=R/max(max(R));
 
-% 打印寻找结果
+% 寻找最大的概率点，并记录
 for i=1:m
     for j=1:n
         if(R(i,j)>0.95) 
-            k=i;l=j;break;
+            kx=i;ky=j;break;
         end
     end
 end
-figure(6); plot(R(k,:)),grid;
-figure(7); plot(R(:,l)),grid;
+% 绘制统计图
+figure(6); plot(R(kx,:)),grid;
+figure(7); plot(R(:,ky)),grid;
 
-figure(10);imshow(I(k:k+nj,l:l+mj));
+% 显示I中的字母位置
+figure(10);imshow(I(kx:kx+nj,ky:ky+mj));
 title('Search Image result');
 
 
