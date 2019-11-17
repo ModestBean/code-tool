aaaeb94217a26329fb29485abbe366aa5c253ad4
ModestBean <yindou97@163.com>
2019-11-17 15:25:01 +0800

    [update] lab6Version3添加详细讲解

--- image_processing/lab6_1101/cutting_color_hsv.m
+++ image_processing/lab6_1101/cutting_color_hsv.m
@@ -1,8 +1,11 @@
-% @author 憨豆酒 YinDou yindou97@163.com https://github.com/ModestBean
-% @description 提取目标颜色值
-% @version1 20191101 修改人：YinDou 提取HSV图像中重点信息（存在杂质）
-% @version2 20191105 修改人：YinDou 修改变量，易读
-% @version2 20191117 修改人：YinDou 去掉杂质，提取更准确
+% @Author 憨豆酒 YinDou yindou97@163.com https://github.com/ModestBean
+% @Description 提取目标颜色值
+% @Version1 20191101 修改人：YinDou 
+% 完成内容：提取HSV图像中重点信息（存在其他颜色）。
+% @Version2 20191105 修改人：YinDou 
+% 完成内容：修改变量定义，使程序更加易读。
+% @Version3 20191117 修改人：YinDou 
+% 完成内容：根据橙色的HSB范围进行过滤其他颜色，相当于对Version1的改进
 
 rgb = imread('onion.png');
 hsv = rgb2hsv(rgb);
@@ -10,13 +13,14 @@ hsv = rgb2hsv(rgb);
 figure(1), imshow(hsv2rgb(hsv));
 target_pixel_H_min = 0.0833; % 目标H通道最小值
 target_pixel_H_max = 0.15; % 目标H通道最大值
-target_pixel_S_min = 0.4; % 目标S通道
+target_pixel_S_min = 0.4; % 目标S通道最小值
 target_pixel_B = 1; % 目标B通道
+
 background = [0.0, 0.0, 0.5];
 w = 0.001;
 for i = 1 : n
     for j = 1 : m
-      H = hsv(i, j, 1); % 原图H
+        H = hsv(i, j ,1); % 原图H
       S = hsv(i, j, 2); % 原图S
       B = hsv(i, j, 3); % 原图B
       % 过滤掉其他颜色，只保留橙色 橙色B为1，H范围是30~54，S范围为40~100
