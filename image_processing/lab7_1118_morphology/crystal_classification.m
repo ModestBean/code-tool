408d496dae7bab0b8401ae9e13dd5851883c56dc
ModestBean <yindou97@163.com>
2019-11-19 23:02:12 +0800

    [update] 根据亮点元素进行晶体分类

--- image_processing/lab7_1118_morphology/crystal_classification.m
+++ image_processing/lab7_1118_morphology/crystal_classification.m
@@ -1,16 +1,19 @@
 % @Author 憨豆酒 YinDou yindou97@163.com https://github.com/ModestBean
 % @Description 提取目标颜色值
-% @Version1 20191101 修改人：YinDou 
+% @Version1 20191118 修改人：YinDou 
 % 完成内容：完成晶体分类
+% @Version2 20191119 修改人：YinDou 
+% 完成内容：通过分析晶体亮度像素点在像素中的含量在对晶体进行分类
 clc;
 close all;
 clear;
-img = imread('./Data/hight.png'); % 读取原图像
+img = imread('./Data/low.png'); % 读取原图像
 figure, imshow(img), title('原始图像');
 grayimg = rgb2gray(img);
 BWimg = grayimg;
 [width, height] = size(grayimg);
 figure, imshow(grayimg), title('灰度图像');
+crystal_length = length(grayimg(grayimg ~= 255)); % 获取晶体总像素点个数
 % 二值化，< 80 纯白色 >= 80 纯黑色  
 T1 = 80;
 for i = 1 : width
@@ -28,15 +31,9 @@ se = strel('disk', 5);
 BWimg = imclose(BWimg, se);
 BWimg = imopen(BWimg, se);
 figure, imshow(BWimg), title('闭关运算后的图像');
+crystal__no_light_length = length(BWimg(BWimg == 255)); % 获取发光像素点个数
 % 统计标注连通域
 [mark_image, num] = bwlabel(BWimg, 4); 
-% 参考博客https://blog.csdn.net/wanrenwangxuejing/article/details/25108191
-% bwlabel 寻找连通区域，4连通是指，如果像素的位置在其他像素相邻的上、下、左或右，则认为他们是连接着的
-% num 表示连通区域的个数
-% l是大小和BWing一样的图像数组，里面存放着对bwing图像的标签值（即判定为连通后，在L矩阵中标记出来）
-% regionprops 介绍
-% 参考 ：https://blog.csdn.net/langb2014/article/details/49886787
-% 返回值STATS是一个长度为max(L(:))的结构数组，结构数组的相应域定义了每一个区域相应属性下的度量
 status = regionprops(mark_image, 'BoundingBox');
 centroid = regionprops(mark_image, 'Centroid');
 figure;
@@ -55,6 +52,9 @@ for i = 1 : num
     all_area = all_area + area_result.Area;
     all_perimeter = all_perimeter + perimeter_result.Perimeter;
 end
+% 计算发光比值
+proportion = ((crystal_length - crystal__no_light_length) / crystal_length) * 100;
 
+fprintf('all_proportion = %f%%\n', proportion);
 fprintf('all_area = %f\n', all_area);
 fprintf('all_perimeter = %f\n', all_perimeter);
