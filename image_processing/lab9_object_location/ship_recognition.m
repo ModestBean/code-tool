1db379677009d392058c70429c8a04ed4c463e7b
ModestBean <yindou97@163.com>
2019-12-13 23:17:14 +0800

    [add] lab9形态学处理

--- image_processing/lab9_object_location/ship_recognition.m
+++ image_processing/lab9_object_location/ship_recognition.m
@@ -5,5 +5,14 @@
 clc;
 close all;
 clear;
-BW = imread('ship_ocean.jpg');
-figure, imshow(BW), title('原始图像');
+origin_image = imread('ship_ocean.jpg');
+figure, imshow(origin_image, 'InitialMagnification', 'fit'), title('原始图像');
+gray_image = rgb2gray(origin_image);
+binary_image = imbinarize(gray_image, 0.77);
+figure,imshow(binary_image, 'InitialMagnification', 'fit'), title('二值化图像');
+% 先闭运算 再开运算
+se = strel('disk', 5);
+morphology_image = imclose(binary_image, se);
+morphology_image = imopen(morphology_image, se);
+figure, imshow(morphology_image, 'InitialMagnification', 'fit'), title('形态学图像');
+
